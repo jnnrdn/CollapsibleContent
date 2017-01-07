08dcf88b25d254775fb2a651f90d89102a9c7270
Jenny <jenny@webbviken.se>
2017-01-07 18:42:33 +0100

    First commit.

---
+++ bootstrap.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Collapsible Content plugin
+ *
+ * @package     JennyRyden\CollapsibleContent
+ * @author      jryden
+ * @copyright   2016 Jenny Ryden
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Collapsible Content
+ * Plugin URI:  https://github.com/webjen/collapsible-content
+ * Description: A plugin that allows you to show and hide pieces of content. Click on the teaser to open, click again to close. For FAQ's, Q & A's, hints, marketing teasers and more.
+ * Version:     1.0.0
+ * Author:      jryden
+ * Author URI:  https://jennyryden.com
+ * Text Domain: collapsible-content
+ * Domain Path: /languages
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Requires WP: 4.7
+ * Requires PHP: 5.5
+ */
+
+/*
+	Collapsible Content is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 2 of the License, or
+	any later version.
+
+	Collapsible Content is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Collapsible Content. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+namespace JennyRyden\CollapsibleContent;
+
+function plugin_launch() {
+
+}
---
+++ src/plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Description
+ *
+ * @package     ${NAMESPACE}
+ * @since       1.0.0
+ * @author      jryden
+ * @link        https://webbviken.se
+ * @license     GNU General Public License 2.0+
+ */
